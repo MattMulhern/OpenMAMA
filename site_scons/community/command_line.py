a960f08a2a2c1ba123656029e6b093f49c5828be
Matt Mulhern <matt@openmama.org>
2015-12-22 10:32:45 +0000

    SCONS: including oea lib and include paths when building with_entitlements
    Signed-off-by: Matt Mulhern <matt@openmama.org>

--- site_scons/community/command_line.py
+++ site_scons/community/command_line.py
@@ -25,6 +25,7 @@ def get_command_line_opts( host, products, VERSIONS ):
        BoolVariable('with_testtools','Build with test tools',False),
        BoolVariable('with_examples','Build with test tools',True),
        BoolVariable('entitled','Whether the build is entitled or unentitled',False),
+       PathVariable('entitle_home','Path to entitlements home',None, PathVariable.PathIsDir),
        PathVariable('gtest_home','Path to Google Test home',None, PathVariable.PathIsDir),
        PathVariable('junit_home','Path to Junit home',None, PathVariable.PathIsDir),
        ListVariable('middleware','Middleware(s) to be compiled in', 'avis', names = ['avis', 'qpid'] ),
